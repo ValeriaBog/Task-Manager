2b751850bfc97a584e3cc1bbcefd217cbe6018d6
ValeriaBog <valeriabog@yandex.ru>
2023-10-13 10:21:20 +0300

    Update header component

--- src/app/header/header.tsx
+++ src/app/header/header.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import {AppBar, Button, IconButton, LinearProgress, Toolbar, Typography} from "@mui/material";
-import {Menu} from "@mui/icons-material";
+import {
+    AppBar,createTheme,
+    CssBaseline,
+    IconButton,
+    LinearProgress,
+    ThemeProvider,
+    Toolbar,
+    Typography
+} from "@mui/material";
 import {useSelector} from "react-redux";
 import {selectIsLoggedIn} from "../../features/auth/model/auth.selectors";
 import {selectAppStatus} from "../app.selectors";
 import {useActions} from "../../common/hooks";
 import {authThunks} from "../../features/auth/model/auth.slice";
+import {Logo} from "../../assets/logo";
+import s from './header.module.css'
+import {Exit} from "../../assets/exit";
 
 export const Header = () => {
 
@@ -14,22 +24,38 @@ export const Header = () => {
     const { logout } = useActions(authThunks);
     const logoutHandler = () => logout();
 
+    const theme = createTheme({
+        typography: {
+            fontFamily: "Roboto",
+            fontSize: 20,
+        },
+    });
+
     return (
         <>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton edge="start" color="inherit" aria-label="menu">
-                        <Menu />
-                    </IconButton>
-                    <Typography variant="h6">News</Typography>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+            <AppBar position="static" color={'default'}>
+                <Toolbar className={s.container}>
+                    <div className={s.logo}>
+                        <IconButton edge="start" color="inherit" aria-label="menu" >
+                            <Logo/>
+                        </IconButton>
+                        <Typography variant="h6" style={{ fontFamily: "Roboto, sans-serif", color: '#09244B'}}>TRELLO</Typography>
+                    </div>
+
                     {isLoggedIn && (
-                        <Button color="inherit" onClick={logoutHandler}>
-                            Log out
-                        </Button>
+                        <IconButton edge="end" color="inherit" aria-label="menu" onClick={logoutHandler}>
+                            <Exit/>
+                        </IconButton>
+                        // <Button color="inherit" onClick={logoutHandler} className={s.buttonLogOut}>
+                        //     Log out
+                        // </Button>
                     )}
                 </Toolbar>
-                {status === "loading" && <LinearProgress />}
+                {status === "loading" && <LinearProgress color={"secondary"}/>}
             </AppBar>
+            </ThemeProvider>
         </>
     );
 };
--- src/features/auth/ui/login/login.tsx
+++ src/features/auth/ui/login/login.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState} from "react";
 import { FormikHelpers, useFormik } from "formik";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, Grid, TextField } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormLabel,
+  Paper,
+  TextField, Typography
+} from "@mui/material";
 import { useActions } from "common/hooks";
 import { selectIsLoggedIn } from "features/auth/model/auth.selectors";
 import { authThunks } from "features/auth/model/auth.slice";
@@ -17,6 +26,11 @@ export const Login = () => {
 
   const { login } = useActions(authThunks);
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const [expanded, setExpanded] = useState<boolean>(false);
+
+  const handleChange = () => {
+        setExpanded(!expanded);
+      };
 
   const formik = useFormik({
     validate: (values) => {
@@ -28,7 +42,7 @@ export const Login = () => {
       }
 
       if (!values.password) {
-        errors.password = "Required";
+        errors.password = "Password is required";
       } else if (values.password.length < 3) {
         errors.password = "Must be 3 characters or more";
       }
@@ -56,42 +70,50 @@ export const Login = () => {
   }
 
   return (
-    <Grid container justifyContent="center">
-      <Grid item xs={4}>
-        <form onSubmit={formik.handleSubmit}>
+        <form onSubmit={formik.handleSubmit} className={s.container}>
+          <Paper elevation={3} className={s.list}>
           <FormControl>
             <FormLabel>
-              <p>
-                To log in get registered{" "}
-                <a href={"https://social-network.samuraijs.com/"} target={"_blank"} rel="noreferrer">
-                  here
-                </a>
-              </p>
-              <p>or use common test account credentials:</p>
-              <p> Email: free@samuraijs.com</p>
-              <p>Password: free</p>
+              <Typography variant="h4" className={s.title}>Sign in</Typography>
             </FormLabel>
             <FormGroup>
-              <TextField label="Email" margin="normal" {...formik.getFieldProps("email")} />
-              {formik.touched.email && formik.errors.email && <p className={s.error}>{formik.errors.email}</p>}
-              <TextField type="password" label="Password" margin="normal" {...formik.getFieldProps("password")} />
-              {formik.touched.password && formik.errors.password && <p className={s.error}>{formik.errors.password}</p>}
-              <FormControlLabel
-                label={"Remember me"}
-                control={<Checkbox {...formik.getFieldProps("rememberMe")} checked={formik.values.rememberMe} />}
-              />
-              <Button
-                type={"submit"}
-                variant={"contained"}
-                disabled={!(formik.isValid && formik.dirty)}
-                color={"primary"}
-              >
-                Login
-              </Button>
+              <TextField label="Email" margin="normal" variant="filled" color={"secondary"} {...formik.getFieldProps("email")} />
+              {formik.touched.email && formik.errors.email && <Typography className={s.error}>{formik.errors.email}</Typography>}
+              <TextField type="password" label="Password" variant="filled" color="secondary" margin="normal" {...formik.getFieldProps("password")} />
+              {formik.touched.password && formik.errors.password && <Typography className={s.error}>{formik.errors.password}</Typography>}
+              <div className={s.check}>
+                <FormControlLabel
+                    label={"Remember me"} color={"secondary"}
+                    control={<Checkbox color="secondary" {...formik.getFieldProps("rememberMe")} checked={formik.values.rememberMe} />}
+                />
+                <div>
+                  <button className={s.datasForSignUp} onClick={handleChange}
+                  >New to TRELLO?</button>
+                  {expanded && <div className={s.info}>
+                    <Typography variant="h6" style={{textAlign: 'center'}}>
+                      Use common test account
+                    </Typography>
+                    <Typography variant="h6" style={{fontSize: 15, marginLeft: 15}}>
+                      Email: free@samuraijs.com
+                    </Typography>
+                    <Typography variant="h6" style={{fontSize: 15, marginLeft: 15}}>
+                      Password: free
+                    </Typography>
+                  </div>}
+                </div>
+              </div>
+
+                <Button
+                    type={"submit"}
+                    variant={"contained"}
+                    disabled={!(formik.isValid && formik.dirty)}
+                    color={"secondary"}
+                >
+                  Login
+                </Button>
             </FormGroup>
           </FormControl>
+            </Paper>
         </form>
-      </Grid>
-    </Grid>
   );
 };
